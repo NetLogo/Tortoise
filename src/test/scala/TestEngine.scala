47be69d4144a74f7b01e92fb48423cd87f9dc4c0
TheBizzle <jason.bertsche@gmail.com>
2013-11-14 15:38:13 -0600

    Allowing for publishing to BinTray (through `publish-versioned`)

--- src/test/scala/TestEngine.scala
+++ src/test/scala/TestEngine.scala
@@ -2,7 +2,7 @@
 
 package org.nlogo.tortoise
 
-import rhino.Rhino
+import rhino.{ Rhino => MRhino }
 import org.scalatest.FunSuite
 
 // just basic smoke tests that basic Tortoise engine functionality is there,
@@ -12,13 +12,13 @@ class TestEngine extends FunSuite {
 
   test("can eval a number literal") {
     assertResult(Double.box(2)) {
-      val rhino = new Rhino
+      val rhino = new MRhino
       rhino.eval("2.0")
     }
   }
 
   test("empty world") {
-    val rhino = new Rhino
+    val rhino = new MRhino
     rhino.eval("world = new World(-1, 1, -1, 1)")
     rhino.eval("world.clearAll()")
     assertResult(Double.box(9)) {
