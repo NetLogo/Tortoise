448092e0e850e4c2fd51a694fbc56789b213c9f6
TheBizzle <jason.bertsche@gmail.com>
2014-05-01 18:15:42 -0500

    Making annoying type parameter into a type member

--- src/main/scala/jsengine/JSEngine.scala
+++ src/main/scala/jsengine/JSEngine.scala
@@ -12,7 +12,9 @@ trait JSEngine {
   def eval(js: String): String
 }
 
-trait JSEngineCompanion[+T <: JSEngine] {
+trait JSEngineCompanion {
+
+  protected type T <: JSEngine
 
   def cleanSlate: T
   def version:    String
@@ -39,17 +41,20 @@ object JSEngine {
     override def eval(js: String): String = engine.eval(js).toString
   }
 
-  object Nashorn extends JSEngineCompanion[NashornEngine] {
+  object Nashorn extends JSEngineCompanion {
+    override protected type T = NashornEngine
     override def cleanSlate = new NNashorn
     override def version    = cleanSlate.engine.versionNumber
   }
 
-  object SpiderMonkey extends JSEngineCompanion[SpiderMonkeyEngine] {
+  object SpiderMonkey extends JSEngineCompanion {
+    override protected type T = SpiderMonkeyEngine
     override def cleanSlate = new NSpiderMonkey
     override def version    = cleanSlate.engine.versionNumber
   }
 
-  object V8 extends JSEngineCompanion[V8Engine] {
+  object V8 extends JSEngineCompanion {
+    override protected type T = V8Engine
     override def cleanSlate = new NV8
     override def version    = cleanSlate.engine.versionNumber
   }
