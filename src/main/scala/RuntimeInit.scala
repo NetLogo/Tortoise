38888ccec20677cf22e747cc54e253fbb9202ff5
TheBizzle <jason.bertsche@gmail.com>
2014-05-29 14:20:52 -0500

    Making `RuntimeInit` more so to my liking

--- src/main/scala/RuntimeInit.scala
+++ src/main/scala/RuntimeInit.scala
@@ -2,69 +2,39 @@
 
 package org.nlogo.tortoise
 
-import org.nlogo.{ core, api }
+import
+  org.nlogo.{ api, core, tortoise },
+    api.{ AgentVariables, Program, ShapeList },
+    core.Model,
+    tortoise.json.JSONSerializer
 
 // RuntimeInit generates JavaScript code that does any initialization that needs to happen
 // before any user code runs, for example creating patches
 
-class RuntimeInit(program: api.Program, model: core.Model) {
+class RuntimeInit(program: Program, model: Model) {
 
-  import scala.collection.JavaConverters._
-  import org.nlogo.tortoise.json.JSONSerializer
+  def init: String = {
 
-  def init = {
-    def shapeList(shapes: api.ShapeList) =
-      if(shapes.getNames.isEmpty)
-        "{}"
-      else
-        JSONSerializer.serialize(shapes)
-    val turtleShapesJson = shapeList(CompilerService.parseTurtleShapes(model.turtleShapes.toArray))
-    val linkShapesJson = shapeList(CompilerService.parseLinkShapes(model.linkShapes.toArray))
-    val view = model.view
-    import view._
-    val workspaceArgs = s"$globalNames, $interfaceGlobalNames, $minPxcor, $maxPxcor, $minPycor, $maxPycor, $patchSize, " +
-      s"$wrappingAllowedInX, $wrappingAllowedInY, $turtleShapesJson, $linkShapesJson"
+    val turtlesOwn = {
+      val builtinCount = AgentVariables.getImplicitTurtleVariables.size
+      vars(program.turtlesOwn.drop(builtinCount), "TurtlesOwn")
+    }
 
-     s"""var workspace     = require('engine/workspace')($breedObjects)($workspaceArgs);
-        |var AgentSet      = workspace.agentSet;
-        |var BreedManager  = workspace.breedManager;
-        |var LayoutManager = workspace.layoutManager;
-        |var LinkPrims     = workspace.linkPrims;
-        |var Prims         = workspace.prims;
-        |var Updater       = workspace.updater;
-        |var world         = workspace.world;
-        |var TurtlesOwn    = world.turtlesOwn;
-        |var PatchesOwn    = world.patchesOwn;
-        |var LinksOwn      = world.linksOwn;
-        |
-        |var Call       = require('engine/call');
-        |var ColorModel = require('engine/colormodel');
-        |var Dump       = require('engine/dump');
-        |var Exception  = require('engine/exception');
-        |var Link       = require('engine/link');
-        |var LinkSet    = require('engine/linkset');
-        |var Nobody     = require('engine/nobody');
-        |var PatchSet   = require('engine/patchset');
-        |var Tasks      = require('engine/tasks');
-        |var Trig       = require('engine/trig');
-        |var Turtle     = require('engine/turtle');
-        |var TurtleSet  = require('engine/turtleset');
-        |var Type       = require('engine/typechecker');
-        |
-        |var AgentModel     = require('integration/agentmodel');
-        |var Denuller       = require('integration/denuller');
-        |var notImplemented = require('integration/notimplemented');
-        |var Random         = require('integration/random');
-        |var StrictMath     = require('integration/strictmath');
-        |
-        |$turtlesOwn$patchesOwn$linksOwn""".stripMargin
+    val patchesOwn = {
+      val builtinCount = AgentVariables.getImplicitPatchVariables.size
+      vars(program.patchesOwn.drop(builtinCount), "PatchesOwn")
+    }
+
+    val linksOwn = {
+      val builtinCount = AgentVariables.getImplicitLinkVariables.size
+      vars(program.linksOwn.drop(builtinCount), "LinksOwn")
+    }
+
+    genInitJS(genBreedObjects, genWorkspaceArgs, turtlesOwn, patchesOwn, linksOwn)
+
   }
 
-  private def globalNames = mkJSArrStr(program.globals map (_.toLowerCase) map wrapInQuotes)
-
-  private def interfaceGlobalNames = mkJSArrStr(program.interfaceGlobals map (_.toLowerCase) map wrapInQuotes)
-
-  private def breedObjects = {
+  private def genBreedObjects: String = {
     val breedObjs =
       program.breeds.values.map {
         b =>
@@ -76,23 +46,35 @@ class RuntimeInit(program: api.Program, model: core.Model) {
     mkJSArrStr(breedObjs)
   }
 
-  // tell the runtime how many *-own variables there are
-  val turtleBuiltinCount =
-    api.AgentVariables.getImplicitTurtleVariables.size
-  val patchBuiltinCount =
-    api.AgentVariables.getImplicitPatchVariables.size
-  val linkBuiltinCount =
-    api.AgentVariables.getImplicitLinkVariables.size
-  def turtlesOwn =
-    vars(program.turtlesOwn.drop(turtleBuiltinCount), "TurtlesOwn")
-  def patchesOwn =
-    vars(program.patchesOwn.drop(patchBuiltinCount), "PatchesOwn")
-  def linksOwn =
-    vars(program.linksOwn.drop(linkBuiltinCount), "LinksOwn")
+  private def genWorkspaceArgs: String = {
 
-  private def vars(s: Seq[String], initPath: String) =
-    if (s.nonEmpty) s"$initPath.init(${s.size});\n"
-    else ""
+    def shapeList(shapes: ShapeList): String = {
+      import scala.collection.JavaConverters.asScalaSetConverter
+      if (shapes.getNames.asScala.nonEmpty)
+        JSONSerializer.serialize(shapes)
+      else
+        "{}"
+    }
+
+    val globalNames          = mkJSArrStr(program.globals          map (_.toLowerCase) map wrapInQuotes)
+    val interfaceGlobalNames = mkJSArrStr(program.interfaceGlobals map (_.toLowerCase) map wrapInQuotes)
+
+    val turtleShapesJson = shapeList(CompilerService.parseTurtleShapes(model.turtleShapes.toArray))
+    val linkShapesJson   = shapeList(CompilerService.parseLinkShapes(model.linkShapes.toArray))
+
+    val view = model.view
+    import view._
+
+    s"$globalNames, $interfaceGlobalNames, $minPxcor, $maxPxcor, $minPycor, $maxPycor, $patchSize, " +
+      s"$wrappingAllowedInX, $wrappingAllowedInY, $turtleShapesJson, $linkShapesJson"
+
+  }
+
+  private def vars(s: Seq[String], initPath: String): String =
+    if (s.nonEmpty)
+      s"$initPath.init(${s.size});\n"
+    else
+      ""
 
   private def wrapInQuotes(str: String): String =
     s"'$str'"
@@ -103,4 +85,40 @@ class RuntimeInit(program: api.Program, model: core.Model) {
     else
       "[]"
 
+  private def genInitJS(breedObjects: String, workspaceArgs: String, turtlesOwn: String, patchesOwn: String, linksOwn: String): String =
+    s"""var workspace     = require('engine/workspace')($breedObjects)($workspaceArgs);
+       |var AgentSet      = workspace.agentSet;
+       |var BreedManager  = workspace.breedManager;
+       |var LayoutManager = workspace.layoutManager;
+       |var LinkPrims     = workspace.linkPrims;
+       |var Prims         = workspace.prims;
+       |var Updater       = workspace.updater;
+       |var world         = workspace.world;
+       |var TurtlesOwn    = world.turtlesOwn;
+       |var PatchesOwn    = world.patchesOwn;
+       |var LinksOwn      = world.linksOwn;
+       |
+       |var Call       = require('engine/call');
+       |var ColorModel = require('engine/colormodel');
+       |var Dump       = require('engine/dump');
+       |var Exception  = require('engine/exception');
+       |var Link       = require('engine/link');
+       |var LinkSet    = require('engine/linkset');
+       |var Nobody     = require('engine/nobody');
+       |var PatchSet   = require('engine/patchset');
+       |var Tasks      = require('engine/tasks');
+       |var Trig       = require('engine/trig');
+       |var Turtle     = require('engine/turtle');
+       |var TurtleSet  = require('engine/turtleset');
+       |var Type       = require('engine/typechecker');
+       |
+       |var AgentModel     = require('integration/agentmodel');
+       |var Denuller       = require('integration/denuller');
+       |var notImplemented = require('integration/notimplemented');
+       |var Random         = require('integration/random');
+       |var StrictMath     = require('integration/strictmath');
+       |
+       |$turtlesOwn$patchesOwn$linksOwn""".stripMargin
+
 }
+
