fc26dc9dc8d00bd8ea650635630db07c0262ef62
skorlir <skorlir@gmail.com>
2014-08-07 16:22:58 -0500

    splice lost coffeescript shim funcs

--- client/anon-fun-wrapper.js
+++ client/anon-fun-wrapper.js
@@ -1,4 +1,10 @@
 var t = (function(){
+  var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __slice = [].slice,
+  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
   %output%
   return [agentmodel, engine, shim, util, nashorn];
 }).call(this);
