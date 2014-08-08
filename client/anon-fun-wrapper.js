4efb2f52f4415ed978a31904387ade9a31a670ef
skorlir <skorlir@gmail.com>
2014-08-08 14:36:51 -0500

    add __modulo definition

--- client/anon-fun-wrapper.js
+++ client/anon-fun-wrapper.js
@@ -3,7 +3,8 @@ var t = (function(){
   __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
   __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   __slice = [].slice,
-  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+  __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; },
+  __modulo = function(a, b) { return (a % b + +b) % b; };
 
   %output%
   return [agentmodel, engine, shim, util, nashorn];
