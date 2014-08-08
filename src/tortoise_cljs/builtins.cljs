f76596db3a2712348fd5566469ed5ce23f5f4ccb
skorlir <skorlir@gmail.com>
2014-08-08 12:00:33 -0500

    simplify alias references

--- src/tortoise_cljs/builtins.cljs
+++ src/tortoise_cljs/builtins.cljs
@@ -12,16 +12,14 @@
 ;; NOTE: this are fns on link; kept here for reference
 ;; renderer expects these; might have to add them to an adapter
 ;; if we even get to that point (7/18/2014)
-(def links-extra [:color :heading :shape :label :label-color :breed
-                  :hidden? :size :midpointx :midpointy])
+(def links-extra [:color :heading :shape :label :label-color
+                  :breed :hidden? :size :midpointx :midpointy])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Aliases                                                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(ns Builtins
-  (:use [engine.core.structure.builtins :only [turtles patches links links-extra]]))
 
-(def turtleBuiltins (clj->js engine.core.structure.builtins/turtles))
-(def patchBuiltins  (clj->js engine.core.structure.builtins/patches))
-(def linkBuiltins     (clj->js engine.core.structure.builtins/links))
-(def linkExtras (clj->js engine.core.structure.builtins/links-extra))
+(def turtleBuiltins (clj->js turtles))
+(def patchBuiltins  (clj->js patches))
+(def linkBuiltins     (clj->js links))
+(def linkExtras (clj->js links-extra))
