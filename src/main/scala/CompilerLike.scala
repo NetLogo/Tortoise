24495d7f9baa52fdf5818f60d0d60e41a86b2852
TheBizzle <jason.bertsche@gmail.com>
2014-08-11 16:36:03 -0500

    Cleaning up `CompilerLike`

--- src/main/scala/CompilerLike.scala
+++ src/main/scala/CompilerLike.scala
@@ -5,15 +5,9 @@ import org.nlogo.nvm.FrontEndInterface._
 import org.nlogo.core.Model
 
 trait CompilerLike {
-  def compileReporter(logo: String,
-                      oldProcedures: ProceduresMap = NoProcedures,
-                      program: Program = Program.empty()): String
-
-  def compileCommands(logo: String,
-                      oldProcedures: ProceduresMap = NoProcedures,
-                      program: Program = Program.empty()): String
-
-  def compileProcedures(code: String): (String, Program, ProceduresMap) = compileProcedures(Model(code = code))
-
-  def compileProcedures(model: Model) : (String, Program, ProceduresMap)
+  def compileReporter(logo: String, oldProcedures: ProceduresMap, program: Program): String
+  def compileCommands(logo: String, oldProcedures: ProceduresMap, program: Program): String
+  def compileProcedures(model: Model): (String, Program, ProceduresMap)
+  def compileProcedures(code: String): (String, Program, ProceduresMap) =
+    compileProcedures(Model(code))
 }
