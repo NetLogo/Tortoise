d10f44431baed70d001037bc61bb10d2a6d7f2f6
TheBizzle <jason.bertsche@gmail.com>
2014-08-11 16:24:55 -0500

    Revert "Add throws declarations to CompilerLike, just to extra explicit"
    This reverts commit efd788dd5a9b397342d32289c5348ba3ad091a9d.
    
    Checked exceptions are abominable.

--- src/main/scala/CompilerLike.scala
+++ src/main/scala/CompilerLike.scala
@@ -1,23 +1,19 @@
 package org.nlogo.tortoise
 
-import org.nlogo.api.{CompilerException, Program}
+import org.nlogo.api.Program
 import org.nlogo.nvm.FrontEndInterface._
 import org.nlogo.core.Model
 
 trait CompilerLike {
-  @throws[CompilerException]
   def compileReporter(logo: String,
                       oldProcedures: ProceduresMap = NoProcedures,
                       program: Program = Program.empty()): String
 
-  @throws[CompilerException]
   def compileCommands(logo: String,
                       oldProcedures: ProceduresMap = NoProcedures,
                       program: Program = Program.empty()): String
 
-  @throws[CompilerException]
   def compileProcedures(code: String): (String, Program, ProceduresMap) = compileProcedures(Model(code = code))
 
-  @throws[CompilerException]
   def compileProcedures(model: Model) : (String, Program, ProceduresMap)
 }
