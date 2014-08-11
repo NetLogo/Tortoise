0f4992a63f02ef7cc92b9deac7367f9eedb844de
skorlir <skorlir@gmail.com>
2014-08-11 14:25:28 -0500

    remove unused "clamp" function

--- src/main/cljs/util/math.cljs
+++ src/main/cljs/util/math.cljs
@@ -1,10 +1,5 @@
 (ns util.math)
 
-(defn clamp [v mn mx]
-  (cond (> v mx) mx
-        (< v mn) mn
-        :default v))
-
 (defn squash [v to precision]
   "squash 'v' to be 'to' if v - to < precision"
   (if (< (Math/abs (- v to))
