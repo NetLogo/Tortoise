943ca5235a604fdc393fe7aa4847c0428d481204
skorlir <skorlir@gmail.com>
2014-08-12 16:11:33 -0500

    get util.math/clamp back for wrapping logic
    This reverts commit 0f4992a63f02ef7cc92b9deac7367f9eedb844de.

--- src/main/cljs/util/math.cljs
+++ src/main/cljs/util/math.cljs
@@ -1,5 +1,10 @@
 (ns util.math)
 
+(defn clamp [v mn mx]
+  (cond (> v mx) mx
+        (< v mn) mn
+        :default v))
+
 (defn squash [v to precision]
   "squash 'v' to be 'to' if v - to < precision"
   (if (< (Math/abs (- v to))
