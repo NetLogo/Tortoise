cab5703319505bc96d34d7a7c3cbd4fdc6319d70
skorlir <skorlir@gmail.com>
2014-08-18 18:25:41 -0500

    refer actual patch-math function for example

--- src/main/cljs/lib/entity.cljs
+++ src/main/cljs/lib/entity.cljs
@@ -40,26 +40,13 @@
 ;; and be returned the fn.
 ;; -- JTT (8/18/14)
 
-(defn patch-math-wrap [p mn mx]
-  ;; squash so that -5.500001 != 5.5 -- JTT (7/28/14)
-  (let [pos p];;(squash-4 p mn)]
-  (cond
-    ;; use >= to consistently return -5.5 for the "seam" of the
-    ;; wrapped shape -- i.e., -5.5 = 5.5, so consistently
-    ;; report -5.5 in order to have equality checks work
-    ;; correctly. -- JTT (7/23/14)
-    (>= pos mx) (-> pos (- mx) (mod (- mx mn)) (+ mn))
-    (< pos mn)  (- mx (-> (- mn pos)
-                          (mod (- mx mn)))) ;; ((min - pos) % (max - min))
-    :default pos)))
-
 (defn wrap
   ([] (fn [e] (let [mnx (get-in e [:edge-bounds :min-pxcor])
                     mxx (get-in e [:edge-bounds :max-pxcor])
                     mny (get-in e [:edge-bounds :min-pycor])
                     mxy (get-in e [:edge-bounds :max-pycor])]
-                {:wrap-x (fn [x] (patch-math-wrap x mnx mxx))
-                 :wrap-y (fn [y] (patch-math-wrap y mny mxy))}))))
+                {:wrap-x (fn [x] (topology.patch-math/wrap x mnx mxx))
+                 :wrap-y (fn [y] (topology.patch-math/wrap y mny mxy))}))))
 
 (defn torus [mnx mxx mny mxy]
   (entity* :torus
