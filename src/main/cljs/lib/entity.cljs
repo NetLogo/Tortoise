2144c1f7ee351b01014674e8fee5e401a5cd8a84
skorlir <skorlir@gmail.com>
2014-08-26 18:28:31 -0500

    in progress instantiation addition for entity defaults

--- src/main/cljs/lib/entity.cljs
+++ src/main/cljs/lib/entity.cljs
@@ -6,10 +6,22 @@
       (conj! e (vec bit)))
     (assoc! e :name (name nm))))
 
+(defn entity-populate-defaults [e]
+  (loop [defaults (:defaults e)
+         n        (dissoc e :defaults)
+         d        (first defaults)]
+    (if (> (count defaults) 0)
+      (recur (drop 1 defaults) (conj n d) (second defaults))
+      n)))
+
 (defn entity-init [e]
   (loop [ifs (:init e)
-         n   {}
-         f (first ifs)]
-    (if (nil? f)
-      n
-      (recur (drop 1 ifs) (conj n (f n)) (second ifs)))))
+         n   (dissoc e :init)
+         f   (first ifs)]
+    (if (not (nil? f))
+      (recur (drop 1 ifs) (conj n (f n)) (second ifs))
+      n)))
+
+(defmacro entity-inst [e]
+  ((comp entity-populate-defaults
+         entity-init) e))
