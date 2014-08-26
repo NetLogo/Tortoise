294c7832d8ed0825c1b0f9d7339be45fb47e0f20
skorlir <skorlir@gmail.com>
2014-08-26 18:31:48 -0500

    scaffolded-out bare entities for patches and turtles

---
+++ src/main/cljs/agents/patch.cljs
@@ -0,0 +1,49 @@
+(ns agents.patch)
+
+;; Components and pieces -- JTT (8/26/14)
+;;
+;; has self-vars
+;; - pcolor
+;; - pxcor
+;; - pycor
+;; - plabel
+;; - plabel-color
+;; contains turtles
+;; - special turtle operations
+;; - - get turtleset
+;; - - "sprout" turtle
+;; - - un/track turtle
+;; coordinates
+;; patch-math
+;; - distance-xy (distance-finders)
+;; - distance
+;; - towardsXY
+;; - get-neighbors-4 (neighborhood-finders)
+;; - get-neighbors
+;; - in-radius (spliced-in-radius)
+;; ask-able
+;; watch-able (by observer)
+;; comparable (by id)
+;; "reset" (return fresh empty patch)
+;; getters and setters for builtins
+
+;; SCAFFOLDING -- JTT (8/26/14)
+(def patch-defaults
+  {:pcolor 0
+   :plabel ''
+   :plabel-color 9.9})
+
+(defn patch [x y]
+  (entity* :patch
+           :defaults patch-defaults
+           :init [(indexed)
+                  (self-vars :patch x y)
+                  (turtle-set)
+                  (life)
+                  (distance-finders)
+                  (towards)
+                  (neighborhood-finders)
+                  (spliced-in-radius)
+                  (ask)
+                  (watch)
+                  (patch-reset)]))
---
+++ src/main/cljs/agents/turtle.cljs
@@ -0,0 +1,72 @@
+(ns agents.turtle)
+
+;; Components and pieces -- JTT (8/26/14)
+;;
+;; self-variables
+;; - breed
+;; - shape
+;; - color
+;; - all the other stuff in builtins
+;; links
+;; current-patch
+;; coordinates
+;; patch-math
+;; - towards (agent)
+;; - towards-xy
+;; - faceXY
+;; - face (agent)
+;; - in-radius
+;; - get-patch
+;; - patch ahead
+;; - patch right and ahead
+;; - patch left and ahead
+;; - get-neighbors-4
+;; - get-neighbors
+;; movement
+;; - fd
+;; - "jump" (go x distance in current direction)
+;; - right (turn right)
+;; - set location (go to x, y)
+;; - move to (another agent's location)
+;; patch interaction
+;; - cohabitating turtles
+;; ask-able
+;; can die
+;; can be hidden
+;; hatch new turtle(s)
+;; watch-able
+;; link interaction
+;; - link-neighbors
+;; - directed and undirected linksets
+;; - link removal
+;;
+
+;; SCAFFOLDING -- JTT (8/26/14)
+(def turtle-defaults
+  {:breed       nil ;; "world.breedManager.turtles()" is the exact default -- JTT (8/26/14)
+   :shape       nil
+   :color       0.0
+   :xcor        0.0
+   :ycor        0.0
+   :size        1.0
+   :heading     0.0
+   :label       ''
+   :label-color 9.9
+   :hidden      false})
+
+(defn turtle [x y]
+  (entity* :turtle
+           :defaults turtle-defaults
+           :init [(indexed)
+                  (self-vars :turtle)
+                  (life)
+                  (distance-finders)
+                  (towards)
+                  (face)
+                  (spliced-in-radius)
+                  (patch-getter)
+                  (movement)
+                  (patches-ahead)
+                  (ask)
+                  (watch)
+                  (links)]))
