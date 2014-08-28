fc1df7b1a47129f9ae794dd72b241769617a5128
skorlir <skorlir@gmail.com>
2014-08-28 14:59:46 -0500

    remove unused entity methods

--- src/main/cljs/lib/entity.cljs
+++ src/main/cljs/lib/entity.cljs
@@ -6,22 +6,10 @@
       (conj! e (vec bit)))
     (assoc! e :name (name nm))))
 
-(defn entity-populate-defaults [e]
-  (loop [defaults (:defaults e)
-         n        (dissoc e :defaults)
-         d        (first defaults)]
-    (if (> (count defaults) 0)
-      (recur (drop 1 defaults) (conj n d) (second defaults))
-      n)))
-
 (defn entity-init [e]
   (loop [ifs (:init e)
-         n   (dissoc e :init)
-         f   (first ifs)]
-    (if (not (nil? f))
-      (recur (drop 1 ifs) (conj n (f n)) (second ifs))
-      n)))
-
-(defmacro entity-inst [e]
-  ((comp entity-populate-defaults
-         entity-init) e))
+         n   {}
+         f (first ifs)]
+    (if (nil? f)
+      n
+      (recur (drop 1 ifs) (conj n (f n)) (second ifs)))))
