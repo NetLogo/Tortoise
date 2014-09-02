dc78686e4e69011a41e4f7146d7a222a2aaca40c
skorlir <skorlir@gmail.com>
2014-09-02 13:12:03 -0500

    remove bad self-manager comment

--- src/main/cljs/agents/singletons/self_manager.cljs
+++ src/main/cljs/agents/singletons/self_manager.cljs
@@ -8,15 +8,7 @@
     (binding [_myself _self
               _self agent]
       (try (f)
-        ;; NOTE:
-        ;; in clojurescript, js/Object = Exception
-        ;; write to log by (.log js/console e)
-        ;;
-        ;; also this error handling is not appropos
-        ;;
-        ;; By the way: if i output to js/console,
-        ;; I will break the tests
-        ;; ... so don't do that. (7/18/2014)
+        ;; Incomplete error handling. -- JTT (8/29/14)
         (catch js/Error error (throw error))))))
 
 (defn self [] @_self)
