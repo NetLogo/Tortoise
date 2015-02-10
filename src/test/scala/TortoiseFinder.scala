93d6675e9ec33432585201a7b504b18ba4fafbf6
TheBizzle <jason.bertsche@gmail.com>
2015-02-10 13:56:30 -0600

    Upgrading to latest Headless version

--- src/test/scala/TortoiseFinder.scala
+++ src/test/scala/TortoiseFinder.scala
@@ -7,7 +7,7 @@ import
     exceptions.TestPendingException
 
 import
-  org.nlogo.headless.lang.{ AbstractFixture, Command, Finder, LanguageTest, NormalMode, TestCommands => TCommands, TestMode, TestReporters => TReporters }
+  org.nlogo.headless.test.{ AbstractFixture, Command, Finder, LanguageTest, NormalMode, CommandTests, TestMode, ReporterTests }
 
 private[tortoise] trait TortoiseFinder extends Finder with BeforeAndAfterAll with BrowserReporter with TestLogger {
 
@@ -59,12 +59,12 @@ private[tortoise] trait TortoiseFinder extends Finder with BeforeAndAfterAll wit
 
 }
 
-class TestReporters extends TReporters with TortoiseFinder {
+class TestReporters extends ReporterTests with TortoiseFinder {
   import Freebies._
   override val freebies = sortingHeteroListReporters ++ evalNotSupportedReporters ++ incErrorDetectReporters
 }
 
-class TestCommands extends TCommands with TortoiseFinder {
+class TestCommands extends CommandTests with TortoiseFinder {
   import Freebies._
   override val freebies = Map[String, String](
     // requires handling of non-local exit (see in JVM NetLogo: `NonLocalExit`, `_report`, `_foreach`, `_run`)
