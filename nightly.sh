2cdf1835cb073297f43cd6cc49a8e82aa9b50128
Robert Grider <robert.grider@northwestern.edu>
2015-02-17 18:00:24 -0600

    Update nightly.sh for new build definitions

--- nightly.sh
+++ nightly.sh
@@ -13,7 +13,7 @@ if [[ "$1" == "--clean" ]] ; then
 fi
 
 dashize(){
-  echo `echo $1 | sed 's/:/-/'`
+  echo `echo $1 | sed 's/:/-/g' | sed 's;/;-;g'`
 }
 
 sbt_task(){
@@ -27,21 +27,23 @@ sbt_test(){
   # here we're using pipes so "-e" isn't enough to stop when something fails.
   # maybe there's an easier way, than I've done it below, I don't know.
   # I suck at shell scripting - ST 2/15/11
-  ./sbt test:$1 2>&1 | tee tmp/nightly/test-$1.txt
-  if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: test:$1"; exit 1; fi
-  echo "*** done: test:$1"
+  ./sbt $1/test:$2 2>&1 | tee tmp/nightly/test-$1-$2.txt
+  if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: $1/test:$2"; exit 1; fi
+  echo "*** done: $1/test:$2"
 }
 
 if [[ "$@" == "" ||  "$@" == "--clean" ]] ; then
   rm -rf tmp/nightly
   mkdir -p tmp/nightly
-  sbt_task test:compile "ensime generate"
-  sbt_test fast
-  sbt_test language
-  sbt_test crawl
-  sbt_task depend
+  sbt_task tortoiseJVM/test:compile "ensime generate"
+  sbt_task tortoiseJS/test:compile
+  sbt_test tortoiseJVM fast
+  sbt_test tortoiseJVM language
+  sbt_test tortoiseJS test
+  sbt_test tortoiseJVM crawl
+  sbt_test netLogoWeb test
+  sbt_task tortoiseJVM/depend
   echo "****** all done!"
 else
   $@
 fi
-
