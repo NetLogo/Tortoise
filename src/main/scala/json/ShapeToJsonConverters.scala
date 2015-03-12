cc0e616a06eb77eaa7deba32fac40c153a0b22c2
TheBizzle <jason.bertsche@gmail.com>
2015-03-12 14:04:08 -0500

    Feature: Serializing link shapes

--- src/main/scala/json/ShapeToJsonConverters.scala
+++ src/main/scala/json/ShapeToJsonConverters.scala
@@ -23,14 +23,32 @@ object ShapeToJsonConverters {
   class VectorShapeConverter(override protected val target: VectorShape) extends ShapeConverter[VectorShape] {
     import scala.collection.JavaConverters._
     import org.nlogo.tortoise.json.ElemToJsonConverters.elem2Json
-    override protected val extraProps = JObject(List(
+    override protected val extraProps = JObject(
       "rotate"   -> JBool(target.isRotatable),
       "elements" -> JArray((target.getElements.asScala map (_.toJsonObj)).toList)
-    ))
+    )
   }
 
   class LinkShapeConverter(override protected val target: LinkShape) extends ShapeConverter[LinkShape] {
-    override protected val extraProps = JObject(List())
+
+    private def lineToJS(index: Int): JObject = {
+      val line = target.getLine(index)
+      JObject(
+        "x-offset"   -> JDecimal(line.xcor),
+        "is-visible" -> JBool(line.isVisible),
+        "dashes"     -> JArray(line.getDashes.toList.map(x => JDecimal(x)))
+      )
+    }
+
+    override protected val extraProps: JObject = {
+      val dirIndicator = target.getDirectionIndicator.asInstanceOf[VectorShape]
+      JObject(
+        "direction-indicator" -> new VectorShapeConverter(dirIndicator).toJsonObj,
+        "curviness"           -> JDecimal(target.curviness),
+        "lines"               -> JArray(0 until 3 map lineToJS toList)
+      )
+    }
+
   }
 
 }
