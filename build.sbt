NetLogo/Tortoise

commit c9904f89ecc03a183130383ee16df41eff0e12f0

Provide ScalaJS-based compiler
      * Introduce a separate 'scalajs' project
      * Refactor repo into separate SBT subprojects
      * Update engine module resolution for new directory structure
      * Use latest NetLogo-Headless version
      * Split out prims into separate traits to appease ScalaJS
      * Update 'nightly.sh' and Depend

build.sbt
@@ -1,71 +1,127 @@
-val root = project in file (".")
+import AddSettings.sbtFiles
+import bintray.Keys.{ bintray => bintrayTask, bintrayOrganization, repository => bintrayRepository }
+import org.scalajs.sbtplugin.cross.{ CrossProject, CrossType }
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.{ fullOptJS, packageJSDependencies }
 
-// Keep this up here so things get published to the correct places
-bintraySettings
+val nlDependencyVersion       = "5.2.0-b5edaf7"
 
-name := "Tortoise"
+val parserJsDependencyVersion = "0.0.1-b5edaf7"
 
-organization := "org.nlogo"
+val commonSettings =
+  // Keep this up here so things get published to the correct places
+  bintraySettings ++
+  Seq(
+    organization  := "org.nlogo",
+    licenses      += ("GPL-2.0", url("http://opensource.org/licenses/GPL-2.0")),
+    version       := "0.1",
+    // Compilation settings
+    crossPaths    := false, // we're not cross-building for different Scala versions
+    scalaVersion  := "2.11.6",
+    scalacOptions ++=
+      "-deprecation -unchecked -feature -Xcheckinit -encoding us-ascii -target:jvm-1.7 -Xlint -Xfatal-warnings -Ywarn-value-discard -language:_".split(" ").toSeq,
+    // Dependencies
+    resolvers           += bintray.Opts.resolver.repo("netlogo", "NetLogoHeadless"),
+    libraryDependencies ++= Seq(
+      "org.nlogo" % "netlogoheadless" % nlDependencyVersion,
+      "org.mozilla" % "rhino" % "1.7R5", // see jsengine/Rhino.scala for more information
+      "org.json4s" %% "json4s-native" % "3.2.11",
+      "org.scalaz" %% "scalaz-core" % "7.1.1",
+      "com.lihaoyi" %% "scalatags" % "0.4.5" % "test",
+      "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+      "org.skyscreamer" % "jsonassert" % "1.2.3" % "test",
+      "org.reflections" % "reflections" % "0.9.9" % "test",
+      // Bring in headless test code/framework for our tests
+      "org.nlogo" % "netlogoheadless" % nlDependencyVersion % "test" classifier "tests"),
+    ivyScala      := ivyScala.value map { _.copy(overrideScalaVersion = true) }, // needed to keep scala.js happy
+    // Path Management
+    resourceDirectory in Compile := (baseDirectory in root).value / "resources" / "main",
+    resourceDirectory in Test    := (baseDirectory in root).value / "resources" / "test",
+    // Build and publication settings
+    isSnapshot                         := true, // Used by the publish-versioned plugin
+    bintrayRepository in bintrayTask   := "Tortoise",
+    bintrayOrganization in bintrayTask := Some("netlogo"),
+    // Logging and Output settings
+    ivyLoggingLevel                 := UpdateLogging.Quiet, // only log problems plz
+    onLoadMessage                   := "",
+    // show test failures again at end, after all tests complete.
+    // T gives truncated stack traces; change to G if you need full.
+    testOptions in Test += Tests.Argument("-oT"))
 
-licenses += ("GPL-2.0", url("http://opensource.org/licenses/GPL-2.0"))
+lazy val root = (project in file("."))
 
-// Used by the publish-versioned plugin
-isSnapshot := true
+lazy val tortoise = CrossProject("tortoise", file("."), new CrossType {
+    override def projectDir(crossBase: File, projectType: String): File =
+      crossBase / projectType
+    override def sharedSrcDir(projectBase: File, conf: String): Option[File] =
+      Some(projectBase / ".." / "tortoise" / "src" / conf / "scala")
+  }).
+  dependsOn(macros % "compile-internal->compile;test-internal->test").
+  settings(PublishVersioned.settings: _*).
+  settings(Depend.settings: _*).
+  settings(commonSettings: _*).
+  jvmConfigure(_.addSbtFiles(file("travis.sbt"))).
+  jvmSettings(FastMediumSlow.settings: _*).
+  jvmSettings(Depend.settings: _*).
+  jvmSettings(
+    name :=  "Tortoise",
+    // this ensures that generated test reports are updated each run
+    (test in Test) <<= (test in Test).dependsOn {
+      Def.task[Unit] {
+        sbt.IO.delete(target.value / "last-test-run-reports")
+      }
+    }).
+  jsSettings(
+    name                                 := "TortoiseJS",
+    artifactPath in (Compile, fullOptJS) := ((crossTarget in (Compile, fullOptJS)).value / "tortoise.js"),
+    skip in packageJSDependencies        := false, // bundles all dependencies in with generated JS
+    testFrameworks                       += new TestFramework("utest.runner.Framework"),
+    libraryDependencies                  ++= {
+      import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport.toScalaJSGroupID
+      Seq(
+        "com.lihaoyi" %%%! "utest" % "0.3.1",
+        "org.nlogo"   %%%! "parser-js" % parserJsDependencyVersion,
+        "com.github.japgolly.fork.scalaz" %%%! "scalaz-core" % "7.1.1")
+    })
 
-version := "0.1"
+lazy val tortoiseJS  = tortoise.js
 
-scalaVersion := "2.11.6"
+lazy val tortoiseJVM = tortoise.jvm
 
-scalacOptions ++=
-  "-deprecation -unchecked -feature -Xcheckinit -encoding us-ascii -target:jvm-1.7 -Xlint -Xfatal-warnings -Ywarn-value-discard -language:_"
-  .split(" ").toSeq
+lazy val macros = CrossProject("macros", file("macros"), CrossType.Pure).
+  settings(commonSettings: _*).
+  settings(
+    libraryDependencies ++= Seq(
+      "org.scala-lang" % "scala-reflect" % scalaVersion.value,
+      "org.scalaz" %% "scalaz-core" % "7.1.1"),
+    ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
+  )
 
-resourceDirectory in Test := baseDirectory.value / "resources" / "test"
+lazy val macrosJS  = macros.js
 
-// show test failures again at end, after all tests complete.
-// T gives truncated stack traces; change to G if you need full.
-testOptions in Test += Tests.Argument("-oT")
+lazy val macrosJVM = macros.jvm
 
-// only log problems plz
-ivyLoggingLevel := UpdateLogging.Quiet
+lazy val cleanGeneratedSources = taskKey[Unit]("deletes generated resources")
 
-// we're not cross-building for different Scala versions
-crossPaths := false
+lazy val netLogoWeb: Project = (project in file("netlogo-web")).
+  dependsOn(tortoiseJVM % "compile-internal->compile;test-internal->test").
+  dependsOn(tortoiseJS  % "compile-internal->compile").
+  settings(commonSettings: _*).
+  settings(
+    name                  := "NetLogoWebJS",
+    libraryDependencies   ++= Seq(
+      "org.nlogo" % "netlogoheadless" % nlDependencyVersion % "test",
+      "org.scalatest" %% "scalatest" % "2.2.1" % "test"),
+    // these tasks force the regeneration of the tortoise.js source on each build
+    resourceGenerators in Compile += Def.task {
+      (fullOptJS in Compile in tortoiseJS).value
+      IO.copy(Seq(((artifactPath in fullOptJS in Compile in tortoiseJS).value, resourceManaged.value / "tortoise.js")))
+      Seq(resourceManaged.value / "tortoise.js")
+    }.taskValue,
+    cleanGeneratedSources          := { IO.delete(resourceManaged.value) },
+    cleanFiles                     <+= resourceManaged,
+    compile                        <<= (compile in Compile) dependsOn(
+      cleanGeneratedSources,
+      managedResources in Compile,
+      clean in tortoiseJS,
+      fullOptJS in Compile in tortoiseJS))
 
-val nlDependencyVersion = "5.2.0-4d484d1"
-
-resolvers += bintray.Opts.resolver.repo("netlogo", "NetLogoHeadless")
-
-libraryDependencies ++= Seq(
-  "org.nlogo" % "netlogoheadless" % nlDependencyVersion,
-  "org.json4s" %% "json4s-native" % "3.2.11",
-  "org.scalaz" %% "scalaz-core" % "7.1.1",
-  "com.lihaoyi" %% "scalatags" % "0.4.5" % "test",
-  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
-  "org.skyscreamer" % "jsonassert" % "1.2.3" % "test",
-  "org.reflections" % "reflections" % "0.9.9" % "test",
-  // Bring in headless test code/framework for our tests
-  "org.nlogo" % "netlogoheadless" % nlDependencyVersion % "test" classifier "tests"
-)
-
-onLoadMessage := ""
-
-unmanagedSourceDirectories in Compile += baseDirectory.value / "scripts"
-
-bintray.Keys.repository in bintray.Keys.bintray := "TortoiseAux"
-
-bintray.Keys.bintrayOrganization in bintray.Keys.bintray := Some("netlogo")
-
-logBuffered in testOnly in Test := false
-
-(test in Test) <<= (test in Test).dependsOn {
-  Def.task[Unit] {
-    sbt.IO.delete(target.value / "last-test-run-reports")
-  }
-}
-
-FastMediumSlow.settings
-
-PublishVersioned.settings
-
-Depend.settings

commit f604880c5a93da9c106ebdfdd624349cfc9035b0

Infrastructure/Bugfix: Publish artifacts to right repo
Regression from c9904f89ecc03a183130383ee16df41eff0e12f0

build.sbt
@@ -38,7 +38,7 @@ val commonSettings =
     resourceDirectory in Test    := (baseDirectory in root).value / "resources" / "test",
     // Build and publication settings
     isSnapshot                         := true, // Used by the publish-versioned plugin
-    bintrayRepository in bintrayTask   := "Tortoise",
+    bintrayRepository in bintrayTask   := "TortoiseAux",
     bintrayOrganization in bintrayTask := Some("netlogo"),
     // Logging and Output settings
     ivyLoggingLevel                 := UpdateLogging.Quiet, // only log problems plz