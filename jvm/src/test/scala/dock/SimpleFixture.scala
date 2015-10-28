e4f2dc79219732188d53290d5a1c88006837ef3c
TheBizzle <jason.bertsche@gmail.com>
2015-10-28 13:30:12 -0500

    Test: Add tests for `date-and-time`

---
+++ jvm/src/test/scala/dock/SimpleFixture.scala
@@ -0,0 +1,58 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/Tortoise
+
+package org.nlogo.tortoise
+package dock
+
+import
+  org.scalatest.fixture.FunSuite
+
+import
+  org.nlogo.core.{ FrontEndInterface, Model, Program },
+      FrontEndInterface.NoProcedures
+
+import
+  jsengine.Nashorn
+
+import
+  CompilerLike.Compilation
+
+trait SimpleSuite extends FunSuite with TestLogger {
+
+  override type FixtureParam = SimpleFixture
+
+  override def withFixture(test: OneArgTest) = {
+
+    val fixture = new SimpleFixture(nashorn)
+
+    loggingFailures(suiteName, test.name, {
+      val outcome = withFixture(test.toNoArgTest(fixture))
+      if (outcome.isFailed || outcome.isExceptional) {
+        testFailed(this.getClass.getName, test.name)
+      }
+      outcome
+    })
+
+  }
+
+}
+
+class SimpleFixture(nashorn: Nashorn) {
+
+  private val procedures = NoProcedures
+  private val program    = Program.empty()
+
+  private val compileCommands = Compiler.compileCommands(_: String, procedures, program)
+  private val compileReporter = Compiler.compileReporter(_: String, procedures, program)
+  private val eval            = nashorn.eval _
+
+  private val compilation = Compilation(Seq(), Seq(), Seq(), Model(), procedures, program)
+  private val js          = Compiler.toJS(compilation)
+
+  eval(js)
+
+  (compileCommands andThen eval)("clear-all random-seed 0")
+
+  def evaluate(nlCode: String): AnyRef =
+    (compileReporter andThen eval)(nlCode)
+
+}
---
+++ jvm/src/test/scala/dock/TestTime.scala
@@ -0,0 +1,45 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/Tortoise
+
+package org.nlogo.tortoise
+package dock
+
+import
+  org.scalacheck.Gen
+
+import
+  org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+class TestTime extends SimpleSuite with GeneratorDrivenPropertyChecks {
+
+  test("date-and-time format") {
+    fixture =>
+
+      val DateRegex = raw"""(\d\d):(\d\d):(\d\d)\.(\d\d\d) (?:AM|PM) (\d\d)-([A-Z][a-z][a-z])-(2\d\d\d)""".r
+      val months    = Set("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+      forAll(Gen.wrap(Gen.const(fixture.evaluate("date-and-time")))) {
+        time =>
+
+          val timeStr = time.toString
+          assert(DateRegex.pattern.matcher(timeStr).matches(),
+                 s"'$timeStr' didn't meet expected format of 'hh:mm:ss.SSS a dd-MMM-yyyy'")
+
+          val DateRegex(hours, minutes, seconds, millis, date, month, year) = timeStr
+
+          assertWithinRange(hours.  toInt, 1,    12)
+          assertWithinRange(minutes.toInt, 0,    59)
+          assertWithinRange(seconds.toInt, 0,    59)
+          assertWithinRange(millis. toInt, 0,    999)
+          assertWithinRange(date.   toInt, 1,    31)
+          assertWithinRange(year.   toInt, 2015, 2999)
+
+          assert(months(month), s"Value '$month' is not within '$months'")
+
+      }
+
+  }
+
+  private def assertWithinRange[T](x: => T, min: T, max: T)(implicit ev: Numeric[T]): Unit =
+    assert(ev.lteq(x, max) && ev.gteq(x, min), s"$x is not within the range [$min, $max].")
+
+}
