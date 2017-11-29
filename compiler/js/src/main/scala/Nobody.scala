aea18c4bfa772038f6f81ac02c25a394184fbac1
LaCuneta <jeremy.baker@northwestern.edu>
2017-11-29 11:04:37 -0600

    Bugfixish: Let `nobody` have a breed so out-of-range `who` numbers do not error

--- compiler/js/src/main/scala/Nobody.scala
+++ compiler/js/src/main/scala/Nobody.scala
@@ -12,9 +12,11 @@ object Nobody {
     Inclusion of `ask` is inspired by the fact that, since a primitive like `create-link-with` can
     return `nobody`, and it can also take an initialization block for the to-be-created thing, either
     the init block must be branched against or `nobody` must ignore it  --JAB (7/18/14)
+    Also include `getBreedName()`, used by `turtlemanager.getTurtleOfBreed()` -- JMB November '17
   */
   def ask(): Unit                 = {}
   def id: Int                     = -1
   def isDead(): Boolean           = true
+  def getBreedName(): String      = "nobody"
   override def toString(): String = "nobody"
 }
