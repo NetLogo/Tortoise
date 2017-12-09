5e19e79c63208f29ca7b35ff6fc9d7b55efcc51c
TheBizzle <jason.bertsche@gmail.com>
2017-12-08 23:04:35 -0600

    Refactor: Provide some handy, Nashorn-only ES6 polyfills

--- netlogo-web/src/main/scala/jsengine/Nashorn.scala
+++ netlogo-web/src/main/scala/jsengine/Nashorn.scala
@@ -33,6 +33,9 @@ class Nashorn {
   for (lib <- jsLibs)
     engine.eval(Resource.asString(lib))
 
+  for (polyfill <- Nashorn.polyfills)
+    engine.eval(polyfill)
+
   // returns anything that got output-printed along the way, and any JSON
   // generated too
   def run(script: String): (String, String) = {
@@ -87,6 +90,88 @@ class Nashorn {
 
   protected def wrapInFunction(script: String): String =
     s"(function () {\n $script \n }).call(this);"
+
+}
+
+object Nashorn {
+  private def polyfills =
+    Seq(
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat#Polyfill
+      """|String.prototype.repeat = function(count) {
+         |  'use strict';
+         |  if (this == null) {
+         |    throw new TypeError('can\'t convert ' + this + ' to object');
+         |  }
+         |  var str = '' + this;
+         |  count = +count;
+         |  if (count != count) {
+         |    count = 0;
+         |  }
+         |  if (count < 0) {
+         |    throw new RangeError('repeat count must be non-negative');
+         |  }
+         |  if (count == Infinity) {
+         |    throw new RangeError('repeat count must be less than infinity');
+         |  }
+         |  count = Math.floor(count);
+         |  if (str.length == 0 || count == 0) {
+         |    return '';
+         |  }
+         |  // Ensuring count is a 31-bit integer allows us to heavily optimize the
+         |  // main part. But anyway, most current (August 2014) browsers can't handle
+         |  // strings 1 << 28 chars or longer, so:
+         |  if (str.length * count >= 1 << 28) {
+         |    throw new RangeError('repeat count must not overflow maximum string size');
+         |  }
+         |  var rpt = '';
+         |  for (var i = 0; i < count; i++) {
+         |    rpt += str;
+         |  }
+         |  return rpt;
+         |};"""
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#Polyfill
+    , """|String.prototype.padStart = function padStart(targetLength,padString) {
+         |  targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+         |  padString = String(padString || ' ');
+         |  if (this.length > targetLength) {
+         |    return String(this);
+         |  }
+         |  else {
+         |    targetLength = targetLength-this.length;
+         |    if (targetLength > padString.length) {
+         |      padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+         |    }
+         |    return padString.slice(0,targetLength) + String(this);
+         |  }
+         |};"""
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+    , """|Object.defineProperty(Object, "assign", {
+         |  value: function assign(target, varArgs) { // .length of function is 2
+         |    'use strict';
+         |    if (target == null) { // TypeError if undefined or null
+         |      throw new TypeError('Cannot convert undefined or null to object');
+         |    }
+         |
+         |    var to = Object(target);
+         |
+         |    for (var index = 1; index < arguments.length; index++) {
+         |      var nextSource = arguments[index];
+         |
+         |      if (nextSource != null) { // Skip over if undefined or null
+         |        for (var nextKey in nextSource) {
+         |          // Avoid bugs when hasOwnProperty is shadowed
+         |          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+         |            to[nextKey] = nextSource[nextKey];
+         |          }
+         |        }
+         |      }
+         |    }
+         |    return to;
+         |  },
+         |  writable: true,
+         |  configurable: true
+         |});"""
+    ).map(_.stripMargin)
 
 }
 
