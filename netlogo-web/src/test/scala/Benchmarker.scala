769120b8fb60ee3ba50c1b3c01801af576fecd90
brotatotes <erichao2018@u.northwestern.edu>
2018-05-16 23:43:19 -0500

    print to console and write to file after each individual benchmark

--- netlogo-web/src/test/scala/Benchmarker.scala
+++ netlogo-web/src/test/scala/Benchmarker.scala
@@ -33,18 +33,18 @@ object Benchmarker extends App {
   // In response to that, you might say, "But they run fine in `TestBenchmarks`!", but I suspect that
   // that is due to those tests not running the standard benchmarking commands (`ca benchmark result`) --JAB (2/4/14)
   private val benchmarkModels =
-    // Seq(
-    //   "BZ Benchmark",
-    //   "FireBig Benchmark",
-    //   "GridWalk Benchmark",
-    //   "Wolf Benchmark",
-    //   "Wealth Benchmark",
-    //   "Erosion Benchmark",
-    //   "Heatbugs Benchmark"
-    // )
-
     Seq(
-      "Connected Chemistry Gas Combustion",
+      "BZ Benchmark",
+      "FireBig Benchmark",
+      "GridWalk Benchmark",
+      "Wolf Benchmark",
+      "Wealth Benchmark",
+      "Erosion Benchmark",
+      "Heatbugs Benchmark"
+    )
+
+    // Seq(
+      // "Connected Chemistry Gas Combustion",
       // "Connected Chemistry Reversible Reaction",
       // "DLA Simple",
       // "DNA Protein Synthesis",
@@ -57,7 +57,7 @@ object Benchmarker extends App {
       // "Plant Speciation",
       // "Team Assembly",
       // "Traffic Grid"
-    )
+    // )
 
   private val engineToEvalMap = Seq(Nashorn, SpiderMonkey, V8).map(engine => engine -> engine.freshEval _).toMap
 
@@ -97,41 +97,34 @@ object Benchmarker extends App {
             |Version:    $versionStr
             |Models:     ${models.mkString(", ")}
             |Iterations: $numIterations
+            |Ticks:      $numTicks
             |Engines:    ${enginesAndEvals.map(_._1.getClass.getSimpleName.init).mkString(", ")}
             |$commentStr
             |
             |""".stripMargin)
 
-  gatherBenchmarks(new File(dir, "models")) map {
-    case Benchmark(name, results) =>
-      results map {
-        case Result(engineName, engineVersion, times) =>
-          val str = s"""$name ($engineName $engineVersion):
-                       |--Average: ${round(times.sum / times.size, 3)} seconds
-                       |--Min:     ${times.min} seconds
-                       |--Max:     ${times.max} seconds
-                       |
-                       |""".stripMargin
-          append(str)
-      }
-  }
+  runBenchmarks(new File(dir, "models")) // Seq[Benchmark] currently unused.
 
-  private def gatherBenchmarks(modelsDir: File): Seq[Benchmark] = {
+  private def runBenchmarks(modelsDir: File): Seq[Benchmark] = {
 
     val modelNameFilePairs = models map (model => (model, pathOfModel(modelsDir, model)))
 
     modelNameFilePairs map {
-      case (name, file) =>
+      case (name, file) => {
         println(s"Running $name")
         val source = Source.fromFile(file)
-        val nlogo  = source.mkString
+        val nlogo  = source.mkString.replaceAll("""\sdisplay\s""", "")
         source.close()
 
         val modelV = CompiledModel.fromNlogoContents(nlogo)
 
         val benchmarkPattern = """(?s).*\n\s*to\s+benchmark.*\n(?s).*\n\s*end(?s).*"""
+        val benchmarkable = nlogo.matches(benchmarkPattern)
+        if (!benchmarkable) {
+          println(s"No `benchmark` procedure found. Running $numTicks ticks.")
+        }
 
-        val jsV = if (nlogo.matches(benchmarkPattern)) {
+        val jsV = if (benchmarkable) {
           for {
             model       <- modelV
             caJS        <- model.compileRawCommand("ca")
@@ -139,7 +132,6 @@ object Benchmarker extends App {
             resultJS    <- model.compileReporter("result")
           } yield s"${model.compiledCode};$caJS;$benchmarkJS;$resultJS;"
         } else {
-          println(s"No Benchmark procedure found. Running $numTicks ticks.")
           for {
             model       <- modelV
             caJS        <- model.compileRawCommand("ca")
@@ -153,13 +145,28 @@ object Benchmarker extends App {
 
         val js = jsV valueOr { case NonEmptyList(head, _) => throw head }
 
-        val results =
-          enginesAndEvals.toSeq map {
-            case (engine, f) => Result(engine.name, engine.version, 1 to numIterations map { _ => f(js).toDouble })
+        val results = enginesAndEvals.toSeq map {
+          case (engine, f) => {
+            val times = 1 to numIterations map (_ => {
+              val time = f(js).toDouble
+              println(time)
+              time
+            })
+            val summary =
+              s"""$name ($engine.name $engine.version):
+                         |--Average: ${round(times.sum / times.size, 3)} seconds
+                         |--Min:     ${times.min} seconds
+                         |--Max:     ${times.max} seconds
+                         |
+                         |""".stripMargin
+            println(summary)
+            append(summary)
+            Result(engine.name, engine.version, times, summary)
           }
+        }
 
-        Benchmark(name, results)
-
+        Benchmark(name, results, results.foldLeft("")(_ + _.summary)) // currently not being used.
+      }
     }
 
   }
@@ -229,7 +236,7 @@ object Benchmarker extends App {
 
   }
 
-  private case class Result(engineName: String, engineVersion: String, times: Seq[Double])
-  private case class Benchmark(name: String, results: Seq[Result])
+  private case class Result(engineName: String, engineVersion: String, times: Seq[Double], summary: String)
+  private case class Benchmark(name: String, results: Seq[Result], summary: String)
 
 }
