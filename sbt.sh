1339fafc30d28a71113bd19d22cf371aecab7abf
LaCuneta <jeremy.baker@northwestern.edu>
2019-02-01 09:54:57 -0600

    Infrastructure: Switch to simpler `sbt.sh` and `.jvmopts` for settings

--- sbt.sh
+++ sbt.sh
@@ -3,34 +3,14 @@
 export JAVA_HOME=$GRAAL_HOME
 
 export PATH=$JAVA_HOME/bin:$PATH
-JAVA=$JAVA_HOME/bin/java
 
-# Most of these settings are fine for everyone
-XSS=-Xss2m
-XMX=-Xmx2048m
-XX=
-ENCODING=-Dfile.encoding=UTF-8
-HEADLESS=-Djava.awt.headless=true
-BOOT=xsbt.boot.Boot
-
-SBT_LAUNCH=$HOME/.sbt/sbt-launch-0.13.8.jar
-URL='http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.8/sbt-launch.jar'
-
-if [ ! -f $SBT_LAUNCH ] ; then
-  echo "downloading" $URL
-  mkdir -p $HOME/.sbt
-  curl -s -S -L -f $URL -o $SBT_LAUNCH || exit
+if [ ! -z $JENKINS_URL ] ; then
+  export SBT_OPTS="-Dsbt.log.noformat=true"
+  echo $JAVA_HOME
+  which java
+  java -version
+  which javac
+  javac -version
 fi
 
-if [ -z $JENKINS_URL ] ; then
-  JAVA_OPTS="$XSS $XMX $XX"
-else
-  JAVA_OPTS="$XSS $XMX $XX -Dsbt.log.noformat=true"
-fi
-
-"$JAVA" \
-    $JAVA_OPTS \
-    $ENCODING \
-    $HEADLESS \
-    -classpath $SBT_LAUNCH \
-    $BOOT "$@"
+sbt "$@"
