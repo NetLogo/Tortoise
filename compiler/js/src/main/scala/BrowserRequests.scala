f3120e42e2a8827bf4fba93358a989b391bd5d0b
LaCuneta <jeremy.baker@northwestern.edu>
2019-02-14 15:12:45 -0600

    Meta: Update NetLogo file version

--- compiler/js/src/main/scala/BrowserRequests.scala
+++ compiler/js/src/main/scala/BrowserRequests.scala
@@ -50,7 +50,7 @@ private[tortoise] trait RequestSharedImplicits {
 
 object ExportRequest extends RequestSharedImplicits {
   val read = Jsonify.reader[JsObject, ExportRequest]
-  final val NlogoFileVersion = "NetLogo 6.0.2"
+  final val NlogoFileVersion = "NetLogo 6.0.4"
 }
 
 case class CompilationRequest(
