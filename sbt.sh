641225373d93cdad85c4443fe68c12afac31c251
LaCuneta <jeremy.baker@northwestern.edu>
2019-02-21 11:50:32 -0600

    Infrastructure: Avoid GraalVM's version of `node`
    Create a temp directory on soft links to the "real" node and npm
    binaries, and add that to the path before the GraalVM binaries.
    Necessary because sometimes node is in a path with a different
    java binary, which conflicts with GraalVM.

--- sbt.sh
+++ sbt.sh
@@ -2,10 +2,33 @@
 
 export JAVA_HOME=$GRAAL_HOME
 
-export PATH=$JAVA_HOME/bin:$PATH
+# GraalVM includes binaries for node and npm that would supercede
+# any installed on the system, which we do not want. -Jeremy B 2/2019
+export NODE=`which node`
+export NODE_HOME=${NODE%/node}
+
+if [ ! -d "node_links" ] ; then
+  mkdir node_links
+fi
+
+# Remove existing links in case they've changed since last run
+if [ -f "node_links/node" ] ; then
+  rm node_links/node
+fi
+
+if [ -f "node_links/npm" ] ; then
+  rm node_links/npm
+fi
+
+ln -s "$NODE_HOME/node" "node_links/node"
+ln -s "$NODE_HOME/npm" "node_links/npm"
+
+export PATH=$PWD/node_links:$JAVA_HOME/bin:$PATH
 
 if [ ! -z $JENKINS_URL ] ; then
   export SBT_OPTS="-Dsbt.log.noformat=true"
+  echo $NODE_HOME
+  node --version
   echo $JAVA_HOME
   which java
   java -version
