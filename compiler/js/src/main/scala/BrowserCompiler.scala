db67aa574889a2be035dd72a54cfbeaa68f416a6
jeremy.baker@northwestern.edu <jeremy.baker@northwestern.edu>
2020-09-10 15:35:02 -0500

    Bugfix/Minor: Use upper case names when incrementally compiling procs

--- compiler/js/src/main/scala/BrowserCompiler.scala
+++ compiler/js/src/main/scala/BrowserCompiler.scala
@@ -124,7 +124,7 @@ class BrowserCompiler {
 
   @JSExport
   def compileProceduresIncremental(command: String, overriding: js.Array[String]): NativeJson = {
-    val overridingSeq: Seq[String] = overriding
+    val overridingSeq: Seq[String] = overriding.map(_.toUpperCase)
     val results: CompiledStringV   = lastCompiledModel.compileProceduresIncremental(command, overridingSeq)
     JsonLibrary.toNative(compileResult2Json.apply(results))
   }
